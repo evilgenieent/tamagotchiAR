743b5e49cd7caad5fe0cfa9dab668ab5b04612f4
IgorSusmelj <igor_7779@hotmail.com>
2014-02-22 12:58:10 +0100

    added several files

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+
+	private bool animate=false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!animation.isPlaying) {
+			animation.Play("Walk");		
+		}
+	}
+}
