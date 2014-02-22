2b728680ae018c338763ff13848e75f6ddcbd59e
Ben <storm.3rr0r@gmail.com>
2014-02-22 18:59:46 +0100

    asdf

--- Assets/Scripts/BallUpdater.cs
+++ Assets/Scripts/BallUpdater.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class BallUpdater : MonoBehaviour {
 
 	public GameObject ball;
+	public PetLogic cat;
+
+	PetLogic logic;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +20,11 @@ public class BallUpdater : MonoBehaviour {
 				transform.position.y,
 				0.0f
 				);
+
+		if (ball.transform.position.magnitude <= 10) {
+			// TODO: prevent spam
+			StartCoroutine(cat.OnPetting());
+		}
 	}
 
 	// Update is called once per frame
