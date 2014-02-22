bff3132c9ff050b03a5874a13010135e19adc905
IgorSusmelj <igor_7779@hotmail.com>
2014-02-22 13:49:28 +0100

    added basic alien model and animation for testing

--- Assets/Scripts/AnimationTest.cs
+++ Assets/Scripts/AnimationTest.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class AnimationTest : MonoBehaviour {
 
 	private bool animate=false;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-	
+		this.animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!animation.isPlaying) {
-			animation.Play("Walk");		
+		if (this.animator) {
+			animator.SetBool("Dead", true);
 		}
 	}
 }
