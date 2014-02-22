d317ae2c1f42017712a4615752c36b22ba2e876a
Ben <storm.3rr0r@gmail.com>
2014-02-22 12:57:33 +0100

    Added skeleton for PetLogic

---
+++ Assets/PetLogic.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetLogic : MonoBehaviour {
+
+	private float hunger;
+	private float tiredness;
+	// whether the pet needs to go to the loo
+	private float excrements;
+	private float health;
+	// whether the pet needs attention or not
+	private float mood;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		// TODO: Here comes the algorithm for the pet behaviour
+	}
+
+	void sendHungry() {
+		// TODO
+	}
+
+	void sendSleepy() {
+		// TODO
+	}
+
+	void sendExcrement() {
+		// TODO
+	}
+
+	void sendDeath() {
+		// TODO
+	}
+
+	void sendAttention() {
+		// TODO
+	}
+
+	public void onFood() {
+		// TODO
+	}
+	
+	public void onAttention() {
+		// TODO
+	}
+}
